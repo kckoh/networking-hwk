32c87b1dd5451de09c104259acd15e7b9ec110ef
MatthewFuchs <82055493+MatthewFuchs@users.noreply.github.com>
2021-10-25 18:24:20 -0700

    Server Code
    I have done some of the server code, I don't know why, but I am not able to access the file at all. Maybe one of you could figure it out. Either way I feel like this is pretty much done, just need to figure out if/why I cannot access the "HelloWorld" file.

---
+++ partA/server.py
@@ -0,0 +1,38 @@
+#import socket module
+import socket
+import sys
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Prepare a server socket
+PORT = 12000
+HOST = socket.gethostbyname(socket.gethostname())
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(5)
+
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        # Send one HTTP header line into socket
+        connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+        connectionSocket.send("\r\n".encode())
+        # Close the client connection socket
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send(b"HTTP/1.1 404 Not found\r\n\r\n")
+        connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+        # Close the client socket
+        connectionSocket.close()
+    
+serverSocket.close()
